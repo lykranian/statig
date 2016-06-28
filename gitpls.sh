fa7bdfe0f7cec85bef09e8f0d9eaf5d1fe89dd30
lyk <lyk@rectilinear.xyz>
2016-06-28 15:20:06 -0700

    repos are properly cloned from url or dir to tmp dir, .txt applied to all files.

--- gitpls.sh
+++ gitpls.sh
@@ -6,8 +6,19 @@
 
 # a static page generator for git repos
 # hosted at https://rectilinear.xyz/git/lyk/gitpls
-# 
-
 
 VERSION="0.0.1"
+ORIGINALWD=`echo $PWD`
+NAME=$(basename $1 .git) # gets the repo name
+RDIR="/tmp/gitpls/repos" # repo storage dir
 
+mkdir -p $RDIR
+cd $RDIR
+rm -rf $NAME
+git clone $1 --quiet 
+echo cloned into $RDIR/$NAME
+rm -rf $RDIR/$NAME/.git
+
+find $RDIR/$NAME/ -type f -exec sh -c 'mv "$0" "${0%}.txt"' {} \;
+
+cd $ORIGINALWD
