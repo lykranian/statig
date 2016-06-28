42a9b16605b58a71935bd2ea566a35aa1abdb999
lyk <lyk@rectilinear.xyz>
2016-06-28 16:46:53 -0700

    generates pages/index.html with correct links to files in pages/files/

--- gitpls.sh
+++ gitpls.sh
@@ -10,15 +10,29 @@
 VERSION="0.0.1"
 ORIGINALWD=`echo $PWD`
 NAME=$(basename $1 .git) # gets the repo name
-RDIR="/tmp/gitpls/repos" # repo storage dir
+GDIR="/tmp/gitpls" # repo storage dir
 
-mkdir -p $RDIR
-cd $RDIR
-rm -rf $NAME
+rm -rf $GDIR/pages/$NAME
+mkdir -p $GDIR/pages/$NAME
+
+mkdir -p $GDIR/repos/
+rm -rf $GDIR/repos/$NAME
+
+cd $GDIR/repos
 git clone $1 --quiet 
-echo cloned into $RDIR/$NAME
-rm -rf $RDIR/$NAME/.git
+printf "cloned into $GDIR/repos/$NAME\n"
+rm -rf $GDIR/repos/$NAME/.git
 
-find $RDIR/$NAME/ -type f -exec sh -c 'mv "$0" "${0%}.txt"' {} \;
+find $GDIR/repos/$NAME/ -type f -exec sh -c 'mv "$0" "${0%}.txt"' {} \;
+#find $GDIR/repos/$NAME/ -type f -exec sh -c "printf `basename` >> ${GDIR%}/pages/${NAME%}/index.txt" {} \;
+find $GDIR/repos/$NAME/ -type f|while read fullname; do
+				    # FNAME=$(basename $fullname .txt) # if your try_files equivalent has $uri.txt
+				    FNAME=$(echo $fullname | sed "s:$GDIR\/repos\/$NAME\/::")
+				    RNAME=$(echo $FNAME | sed "s:.txt$::")
+				    printf "<a href=\"files/$FNAME\">$RNAME</a>\n" >> $GDIR/pages/$NAME/index.html
+				    printf "<br/>" >> $GDIR/pages/$NAME/index.html
+				done
+
+cp -R $GDIR/repos/$NAME $GDIR/pages/$NAME/files
 
 cd $ORIGINALWD
