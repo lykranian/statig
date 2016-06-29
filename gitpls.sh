68b1fe009ced29c616171eee8620ea4024516a4f
lyk <lyk@rectilinear.xyz>
2016-06-28 18:04:10 -0700

    added some css

--- gitpls.sh
+++ gitpls.sh
@@ -24,25 +24,47 @@ rm -rf $GDIR/repos/$NAME/.git # remove the .git folder
 
 find $GDIR/repos/$NAME/ -type f -exec sh -c 'mv "$0" "${0%}.txt"' {} \; # add .txt suffix to all files
 
+# page colors
+LINKCOL="#444444"
+
 # ↓ basic structure for writing to index
 # printf "hello world" >> $GDIR/pages/$NAME/index.html
 printf "<!DOCTYPE HTML>
 <HTML>
   <HEAD>
     <TITLE>gitpls - $NAME</TITLE>
+  <STYLE>
+    body {
+      background-color: #DDDDDD;
+      font-color: #333333;
+    }
+    a:link {
+      color: $LINKCOL;
+    }
+    a:visited {
+      color: $LINKCOL;
+    }
+    a:hover {
+      color: $LINKCOL;
+    }
+    a:active {
+      color: $LINKCOL;
+    }
+  </STYLE>
   </HEAD>
   <BODY>
-    gitpls - $NAME<br/><br/>" >> $GDIR/pages/$NAME/index.html
+    <center>
+      gitpls - $NAME<br/><br/>" >> $GDIR/pages/$NAME/index.html
 
 
 find $GDIR/repos/$NAME/ -type f|while read fullname; do
 				    # FNAME=$(basename $fullname .txt) # if your try_files equivalent has $uri.txt
 				    FNAME=$(echo $fullname | sed "s:$GDIR\/repos\/$NAME\/::")
 				    RNAME=$(echo $FNAME | sed "s:.txt$::")
-				    printf "\n  <a href=\"files/$FNAME\">$RNAME</a>" >> $GDIR/pages/$NAME/index.html
+				    printf "\n      <a href=\"files/$FNAME\">$RNAME</a>" >> $GDIR/pages/$NAME/index.html
 				    printf "<br/>" >> $GDIR/pages/$NAME/index.html
 				done
-printf "  </BODY>
+printf "\n  </BODY>
 </HTML>" >> $GDIR/pages/$NAME/index.html
 cp -R $GDIR/repos/$NAME $GDIR/pages/$NAME/files
 
